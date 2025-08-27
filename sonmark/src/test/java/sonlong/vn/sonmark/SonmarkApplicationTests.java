c08a3902a2e972cbe2b3944dcbc65cbaaba94e8e
Your Name <you@example.com>
2025-08-28 02:59:04 +0700

    first commit

---
+++ sonmark/src/test/java/sonlong/vn/sonmark/SonmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package sonlong.vn.sonmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
